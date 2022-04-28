064e5a805b5ad8a989afe5a24acbe84acaba5f5b
breadman <57912521+breadman123456@users.noreply.github.com>
2022-04-28 10:13:11 -0700

    Update Transaction.java

--- src/main/java/ca/tweetzy/auctionhouse/transaction/Transaction.java
+++ src/main/java/ca/tweetzy/auctionhouse/transaction/Transaction.java
@@ -15,31 +15,17 @@ import java.util.UUID;
  * Usage of any code found within this class is prohibited unless given explicit permission otherwise
  */
 
-@Getter
-@Setter
+@Getter @Setter
 public class Transaction {
 
-	private final UUID id;
-	private final UUID seller;
-	private final UUID buyer;
-	private final String sellerName;
-	private final String buyerName;
+	private final UUID id, seller, buyer;
+	private final String sellerName, buyerName;
 	private final Long transactionTime;
 	private final ItemStack item;
 	private final AuctionSaleType auctionSaleType;
 	private final double finalPrice;
 
-	public Transaction(
-			@NonNull UUID id,
-			@NonNull UUID seller,
-			@NonNull UUID buyer,
-			@NonNull String sellerName,
-			@NonNull String buyerName,
-			long transactionTime,
-			@NonNull ItemStack item,
-			@NonNull AuctionSaleType auctionSaleType,
-			double finalPrice
-	) {
+	public Transaction(@NonNull UUID id, @NonNull UUID seller, @NonNull UUID buyer, @NonNull String sellerName, @NonNull String buyerName, long transactionTime, @NonNull ItemStack item, @NonNull AuctionSaleType auctionSaleType, double finalPrice) {
 		this.id = id;
 		this.seller = seller;
 		this.buyer = buyer;
